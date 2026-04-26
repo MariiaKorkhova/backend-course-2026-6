6dd4776fa89ba1fe6345a2fb605dfa3db14d8222
Mariia Korkhova bc2026-6 <mariia.korkhova@lnu.edu.ua>
2026-04-26 20:47:05 +0000

    Add implementation of post method to main.js

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ const http = require('node:http');
 const fs = require('node:fs');
 const path = require('node:path');
 const { program } = require('commander');
+const multer = require('multer');
 
 program
-    .requiredOption('-h, --host <type>', 'server address')
-    .requiredOption('-p, --port <number>', 'server port')
-    .requiredOption('-c, --cache <path>', 'cache directory path')
+    .requiredOption('-h, --host <type>')
+    .requiredOption('-p, --port <number>')
+    .requiredOption('-c, --cache <path>')
     .configureOutput({
         outputError: (str, write) => {
             if (str.includes("required option")) {
@@ -20,14 +21,63 @@ program
 const options = program.opts();
 
 if (!fs.existsSync(options.cache))
-    {
+{
     fs.mkdirSync(options.cache, { recursive: true });
-    console.log(`creating directory for cache: ${options.cache}`);
+    console.log(`cache directory created: ${options.cache}`);
 }
 
+const upload = multer({ dest: options.cache });
+
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end('server works!\n');
+    const sendResponse = (statusCode, message) => {
+        res.writeHead(statusCode, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(message + '\n');
+    };
+
+    const serveHTML = (filePath) => {
+        fs.readFile(filePath, (err, data) => {
+            if (err)
+            {
+                return sendResponse(500, 'internal server error');
+            }
+
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end(data);
+        });
+    };
+
+    if (req.url === '/register')
+    {
+        if (req.method === 'GET')
+        {
+            serveHTML(path.join(__dirname, 'RegisterForm.html'));
+        
+        }
+        else if (req.method === 'POST')
+        {
+            upload.single('photo')(req, res, (err) => {
+                if (err) {
+                    return sendResponse(500, 'internal server error');
+                }
+
+                if (!req.body || !req.body.inventory_name)
+                {
+                    return sendResponse(400, 'bad request');
+                }
+
+                return sendResponse(201, 'created');
+            });
+            
+        }
+        else
+        {
+            return sendResponse(405, 'method not allowed');
+        }  
+    }
+    else
+    {
+        return sendResponse(404, 'not found');
+    }
 });
 
 server.listen(options.port, options.host, () => {
